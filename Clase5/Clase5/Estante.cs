36a11ff6c3cbc210e9314791c336864dc77352d2
fedefna <fede.fna@hotmail.com>
2018-09-05 22:14:45 -0300

    Add files via upload

---
+++ Clase5/Clase5/Estante.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase5
+{
+    class Estante
+    {
+        private Producto[] productos;
+        private int ubicacionEstante;
+
+        private Estante(int capacidad)
+        {
+            this.productos = new Producto[capacidad];
+        }
+
+        //alta magia, mi constructor publico llama al privado..
+        public Estante(int capacidad, int ubicacion):this(capacidad)
+        {
+            this.ubicacionEstante = ubicacion;
+        }
+
+        public Producto[] getProductos()
+        {
+            return this.productos;
+        }
+
+        public static string mostrarEStante(Estante e)
+        {
+            string retorno="";
+            for (int i=0; i < e.productos.Length; i++)
+            {
+
+            }
+            return retorno;
+        }
+    }
+}
---
+++ Clase5/Clase5/Producto.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase5
+{
+    class Producto
+    {
+        private string codigoDeBarra;
+        private string marca;
+        private float precio;
+
+        public Producto()
+        {
+            this.codigoDeBarra = "";
+            this.marca = "";
+            this.precio = 0;
+        }
+
+        public string getcodigoDeBarra()
+        {
+            return this.codigoDeBarra;
+        }
+
+        public void setCodigoDeBarra(string codigo)
+        {
+            this.codigoDeBarra = codigo;
+        }
+
+        public string getMarca()
+        {
+            return this.marca;
+        }
+
+        public void setMarca(string marca)
+        {
+            this.marca = marca;
+        }
+
+        public float getPrecio()
+        {
+            return this.precio;
+        }
+
+        public void setPrecio(float precio)
+        {
+            this.precio = precio;
+        }
+
+        public void mostrarProducto(Producto prod)
+        {
+            Console.WriteLine("Codigo de barra: "+this.codigoDeBarra+". Marca: "+this.marca+". Precio: "+this.precio);
+        }
+
+        public static explicit operator string(Producto p)
+        {
+            return p.getcodigoDeBarra();
+        }
+
+        public static bool operator == (Producto p1, Producto p2)
+        {
+            bool retorno=false;
+
+            if(p1 is null || p2 is null)
+            {
+                retorno = false;
+            }   else if (p1.codigoDeBarra == p2.codigoDeBarra && p1.marca == p2.marca)
+                {
+                    retorno= true;
+                }
+            return retorno;
+        }
+
+        public static bool operator !=(Producto p1, Producto p2)
+        {
+            return !(p1==p2);
+        }
+
+        public static bool operator ==(Producto p, string marca)
+        {
+            bool retorno = false;
+
+            if (p is null || marca is null)
+            {
+                retorno = false;
+            }
+            else if (p.marca == marca)
+            {
+                retorno = true;
+            }
+            return retorno;
+        }
+
+        public static bool operator !=(Producto p, string marca)
+        {
+            return !(p == marca);
+        }
+    }
+}
